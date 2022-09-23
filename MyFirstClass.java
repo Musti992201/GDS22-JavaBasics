d38e5772fef9e236b6f1dfd9b5d69c08867964ad
Mustafa <Mustafa.Uyar@outlook.at>
2022-09-23 17:16:19 +0200

    My First Class created; args introduced

--- MyFirstClass.java
+++ MyFirstClass.java
@@ -8,5 +8,9 @@ public class MyFirstClass {
 
         System.out.println(Arrays.toString(args)); // durch die toString Methode werden die Namen des Objektes ausgegeben
         System.out.println(args); // es zeigt den Speicherort an und nicht das Objekt selbst
+
+
+        //normalerweise erster Commitbezeichnung: My First Class created: args as input parameter written to console 17:05
+        //Klasse markieren Commit
     }
 }
